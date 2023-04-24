30c1e50380718efabd78ad60978fd5da3efeeb89
@hexlet/cli <support@hexlet.io>
2023-04-24 11:48:51 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -1,59 +1,59 @@
 package exercise;
 
 // BEGIN
+
 import java.util.HashMap;
 import java.util.Map;
 
 
 class App {
 
-public static Map getWordCount(String sentence) {
+    public static Map getWordCount(String sentence) {
 
         String[] words = sentence.split("\\s+");
         Map<String, Integer> wordsToCount = new HashMap<>();
 
         for (String word : words) {
-        if (!wordsToCount.containsKey(word)) {
-        wordsToCount.put(word, 0);
-        }
-        wordsToCount.put(word, wordsToCount.get(word) + 1);
+            if (!wordsToCount.containsKey(word)) {
+                wordsToCount.put(word, 0);
+            }
+            wordsToCount.put(word, wordsToCount.get(word) + 1);
 
         }
         return wordsToCount;
 
-        }
+    }
 
-public static String toString(Map wordsToCount) {
+    public static String toString(Map wordsToCount) {
 
         Map<String, Integer> newWordsToCount = new HashMap(wordsToCount);
-        String stringToWordsCount=null;
+        String stringToWordsCount = null;
 
         for (Map.Entry<String, Integer> wordToCount : newWordsToCount.entrySet()) {
 
 
-        if (wordToCount.getKey().isEmpty()) {
-        stringToWordsCount = "{}";
+            if (wordToCount.getKey().isEmpty()) {
+                stringToWordsCount = "{}";
 
-        } else {
-        StringBuilder stringWords = new StringBuilder();
-        String a = "{";
-        String b = ": ";
-        String c = "\n}";
-
-
-        for (Map.Entry<String, Integer> wordToCountNew : newWordsToCount.entrySet()) {
-        String stringWordKey = wordToCountNew.getKey().toString();
-        String stringWordValue = wordToCountNew.getValue().toString();
-        stringWords.append("\n  " + stringWordKey + b+ stringWordValue
-        );
-        }
-        stringToWordsCount = new String(a + stringWords + c);
+            } else {
+                StringBuilder stringWords = new StringBuilder();
+                String a = "{";
+                String b = ": ";
+                String c = "\n}";
 
 
+                for (Map.Entry<String, Integer> wordToCountNew : newWordsToCount.entrySet()) {
+                    String stringWordKey = wordToCountNew.getKey().toString();
+                    String stringWordValue = wordToCountNew.getValue().toString();
+                    stringWords.append("\n  " + stringWordKey + b + stringWordValue
+                    );
+                }
+                stringToWordsCount = new String(a + stringWords + c);
 
-        }
+
+            }
         }
         return stringToWordsCount;
-        }
-        }
+    }
+}
 //END
