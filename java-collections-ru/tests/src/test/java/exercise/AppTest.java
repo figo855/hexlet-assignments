61b64e9c5fc232260b0ae2725fa4eb9351488d3a
@hexlet/cli <support@hexlet.io>
2023-05-22 11:57:13 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,5 +1,5 @@
 package exercise;
-
+// BEGIN
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
@@ -7,26 +7,39 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
+    private static final List <Integer> LIST10 = List.of(1,2,3,4,5,6,7,8,9,10);
+    private static final List <Integer> LISTEMPTY = new ArrayList<>();
+    private static final int COUNT11 = 11;
+    private static final int COUNT9 = 9;
+    private  static final int COUNT0 = 0;
 
     @Test
-    void testTake() {
-        // BEGIN
-        List <Integer> list10 = List.of(1,2,3,4,5,6,7,8,9,10);
-        List <Integer> listEmpty = new ArrayList<>();
-        int count11 = 11;
-        int count9 = 9;
-        int count0 = 0;
+    void testTake1() {
 
-        assertThat(App.take(list10, count11)).isEqualTo(list10);
 
-        assertThat(App.take(list10, count9)).isEqualTo(list10.subList(0, 8));
+        assertThat(App.take(LIST10, COUNT11)).isEqualTo(LIST10);
+    }
 
-        assertThat(App.take(listEmpty, count9)).isEqualTo(listEmpty);
+    @Test
+    void testTake2() {
 
-        assertThat(App.take(list10, count0)).isEqualTo(listEmpty);
+        assertThat(App.take(LIST10, COUNT9)).isEqualTo(LIST10.subList(0, 8));
+    }
 
-        assertThat(App.take(listEmpty, count0)).isEqualTo(listEmpty);
+    @Test
+    void testTake3() {
+        assertThat(App.take(LISTEMPTY, COUNT9)).isEqualTo(LISTEMPTY);
+    }
 
+    @Test
+    void testTake4() {
+        assertThat(App.take(LIST10, COUNT0)).isEqualTo(LISTEMPTY);
+    }
+
+    @Test
+    void testTake5() {
+        assertThat(App.take(LISTEMPTY, COUNT0)).isEqualTo(LISTEMPTY);
+    }
         // END
     }
-}
+
