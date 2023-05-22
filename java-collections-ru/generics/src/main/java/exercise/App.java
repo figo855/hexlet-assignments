380240a65fc5a0854d109ba237ed8084b57e6a16
@hexlet/cli <support@hexlet.io>
2023-05-22 11:26:07 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -7,22 +7,22 @@ import java.util.Map.Entry;
 
 
 class App {
-    public static List<Map<String, String>> findWhere(List<Map<String, String>> listOfMaps1, Map <String, String> map2) {
-            List<Map<String, String>> matchesList= new ArrayList<>();
-            for (Map<String, String> map1: listOfMaps1) {
-                boolean find = true;
-                for (Map.Entry map2Entry: map2.entrySet()) {
-                    String map1Value = map1.getOrDefault(map2Entry.getKey(), "");
-                    if (map1Value.equals(map2Entry.getValue())) {
-                        find = false;
-                    }
-
-                    }
-                if (find==true) {
-                    matchesList.add(map1);
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> listOfMaps1, Map<String, String> map2) {
+        List<Map<String, String>> matchesList = new ArrayList<>();
+        for (Map<String, String> map1 : listOfMaps1) {
+            boolean find = true;
+            for (Map.Entry map2Entry : map2.entrySet()) {
+                String map1Value = map1.getOrDefault(map2Entry.getKey(), "");
+                if (!map1Value.equals(map2Entry.getValue())) {
+                    find = false;
                 }
+
             }
-            return matchesList;
+            if (find == true) {
+                matchesList.add(map1);
+            }
+        }
+        return matchesList;
     }
 }
 //END
